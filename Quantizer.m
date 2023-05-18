0bce4585558ddd9f4efc0213b633ea3cebba0d94
KareemElozeiri <k.elozeiri@gmail.com>
2023-05-18 23:47:52 +0300

    done with quantizer

--- Quantizer.m
+++ Quantizer.m
@@ -1,4 +1,4 @@
-function [quantized_signal_time_vector, quantized_signal_value_vector] = Quantizer(type, signal_time_vector, signal_value_vector, L, mp)
+function [quantized_signal_time_vector, quantized_signal_value_vector, mean_squared_error] = Quantizer(type, signal_time_vector, signal_value_vector, L, mp)
 %Summary of this function goes here
 %   type -> represents the quantization type (Mid-rise/Mid-tread)
 %   signal_time_vector -> sampling instances of the input signal
@@ -12,8 +12,6 @@ levels = zeros(1, L);
 delta = 2*mp/L;
 
 %generating levels
-
-
 if type=="Mid-rise"
     for i = 1:L
         levels(i) = (i - 0.5) * delta - mp;
@@ -24,16 +22,23 @@ else
     end
 end 
 
+%applying quantization
 for i = 1:length(signal_value_vector)
-    %yet to apply quantization
+    %find the closest quantization level
+    [~, index] = min(abs(signal_value_vector(i) - levels));
+    %quantize the sample to the closest level
+    quantized_signal_value_vector(i) = levels(index);
 end
 
-disp(levels)
+squared_error = (quantized_signal_value_vector - signal_value_vector).^2;
+mean_squared_error = mean(squared_error);
+disp(mean_squared_error);
+
 %plotting the original & quantized signals
 grid on
 plot(signal_time_vector, signal_value_vector);
 hold on
-stairs(quantized_signal_time_vector,levels);
+stairs(quantized_signal_time_vector, quantized_signal_value_vector);
 legend("Original Signal", "Quantized Signal");
 xlabel("Time");
 ylabel("Amplitude");
