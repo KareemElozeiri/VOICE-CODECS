5812cc87d5ee1d9ee8ae12a749486d952ba4ab7f
Mohamed1Hatem <95130017+Mohamed1Hatem@users.noreply.github.com>
2023-05-20 23:58:01 +0300

    a little bit optimized

--- decoder.m
+++ decoder.m
@@ -7,26 +7,31 @@ Amp = zeros(1, length(encoded_msg)/(2*R));
 Time = zeros(1, length(encoded_msg)/(2*R));
 disp(encoded_msg(1:10))
 Str = blanks(R);
-for i=1:length(Amp)
-    b=2*R*(i-1)+1;
-    kk = 1;
-    if type == "Manchester Signaling"
-        
+if type == "Manchester Signaling"
+    for i=1:length(Amp)
+        b=2*R*(i-1)+1;
+        kk = 1;
+
         for j=b:2:(b+2*R-1)
             if (encoded_msg(j) > lamda)  && (encoded_msg(j+1) < lamda)
                 Str(kk) = '1';
             else
                 Str(kk) = '0';
             end
-            disp(encoded_msg(j))
-            disp(encoded_msg(j+1))
-            disp(Str)
             kk = kk +1;
         end 
-        
-        
-    else
-        %perform lternate Mark Inversion Signaling
+        Str = strrep(Str, ' ', '');    
+        Amp(i)= map(Str);
+        T_index = (2*i*R)-1;
+        Time(i)= encoded_msg_time(T_index);
+        Str = blanks(R);
+    end
+    
+else
+    for i=1:length(Amp)
+        b=2*R*(i-1)+1;
+        kk = 1;
+    %perform lternate Mark Inversion Signaling
         for d=b:2:(b+2*R-1)
             if encoded_msg(ceil(d/2)) == 0
                 Str(kk) = '0';
@@ -35,17 +40,26 @@ for i=1:length(Amp)
             end
             kk = kk +1;
         end 
-        
+        Str = strrep(Str, ' ', '');        
+        Amp(i)= map(Str);
+        T_index = (2*i*R)-1;
+        Time(i)= encoded_msg_time(T_index);
+        Str = blanks(R);
     end
-    Str = strrep(Str, ' ', '');
-    disp(length(Str));
-
-    Amp(i)= map(Str);
-    T_index = (2*i*R)-1;
-    Time(i)= encoded_msg_time(T_index);
-    Str = blanks(R);
-    
 end
-plot(Time,Amp);
+    
+    
+
+%figure;
+%Amp = lowpass(Amp,3200);
+%filename = 'Output.mp3';
+
+d=designfilt('lowpassfir','FilterOrder',5,'CutoffFrequency',2000,'SampleRate',1/Time(1));
+Amp = filter(d,Amp);
+sound(Amp,1/Time(1));
+%load handel.mat
+
+%filename = 'handel.wav';
+%audiowrite(filename,Amp,1/Time(1));
 end
 
